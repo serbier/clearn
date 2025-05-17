4423e111fa27a3fd3f401da2d893a2c8b533d9c7
serbier <sergioacr94@gmail.com>
2025-05-17 18:27:11 -0500

    add e27

--- chapter_02/exercises/e27.c
+++ chapter_02/exercises/e27.c
@@ -6,7 +6,7 @@
  * ie: 1 changed into 0 and vice versa. Laving the
  * others unchanged */
 
-unsigned inver(unsigned x, int p, int n);
+unsigned invert(unsigned x, int p, int n);
 
 int main ()
 {
@@ -14,16 +14,18 @@ int main ()
   x = 0b10101010;  // 170
   p = 5;
   n = 3;
-  out = invert(x,p,n); // 0b10111010 = 186
+  out = invert(x,p,n); // 0b1011110 =ó 186
 
-  printf(" x:%d\ty:%d\tp:%d\tn:%d\tout:%d\n", x, y, p, n, out);
+  printf(" x:%d\tp:%d\tn:%d\tout:%d\n", x, p, n, out);
   return 0;
 }
 
-unsigned setbits(unsigned x, int p, int n, unsigned y) {
+unsigned invert(unsigned x, int p, int n) {
   unsigned mask =  ~(~0 << n);
-  unsigned y_bits = (y & mask) << (p+1-n);
-  unsigned x_clean = x & ~(mask << (p+1-n));
-  return x_clean | y_bits;
+  unsigned x_bits = (~x >> (p+1-n)) & mask;
+  unsigned x_clean = x_bits << (p+1-n);
+  unsigned x_bits_clean = ~(mask << (p+1-n));
+  unsigned x_inv = (x & x_bits_clean) | x_clean;
+  return x_inv;
 }
 
